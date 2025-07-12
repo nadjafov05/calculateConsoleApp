f0f7f09bdcf88a8cebd33b979f32a59cc1328795
Perviz Necefov <nadjafovp@gmail.com>
2025-07-12 22:18:01 +0400

    MathUtil and InputUtil added class in console app calculate end2

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,8 @@ import mathUtil.MathUtil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("username: " + args[0]);
-        System.out.println("user password: " + args[1]);
-
-        if (args[0].equals("perviz") && args[1].equals("Leyl@927")) {
             double a = InputUtil.reqemTelebEt("a`ni daxil edin: ");
             double b = InputUtil.reqemTelebEt("b`ni daxil edin: ");
-
 
             char operations = InputUtil.emeliyyatSecimi("emeliyyat secimi edin:" +
                     "\n 1: toplama" +
@@ -21,8 +16,5 @@ public class Main {
 
             double netice = MathUtil.hesablamaEmeliyyati(a, b, operations);
             System.out.println(netice);
-        }else {
-            System.out.println("daxil etdiyiniz istifadeci adi veya parol sehvdir!");
-        }
     }
 }
